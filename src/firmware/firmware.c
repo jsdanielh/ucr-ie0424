f36c36c494f2b778db138996588a49c83799821f
Jose Daniel Hernandez <jsdaniel.h@gmail.com>
2019-03-13 00:22:38 -0600

    Cosmetical change to the firmware code
    Signed-off-by: Jose Daniel Hernandez <jsdaniel.h@gmail.com>

--- src/firmware/firmware.c
+++ src/firmware/firmware.c
@@ -1,17 +1,18 @@
+
 void putuint(unsigned int i) {
 	*(volatile unsigned int*)0x10000000 = i;
 }
 
 void main() {
-	unsigned int numero = 0;
-	unsigned int contador = 0;
-	const unsigned int limite = 1000000;
+	unsigned int number_to_display = 0;
+	unsigned int counter = 0;
+	const unsigned int limit = 1000000;
 
 	while (1) {
-		contador = 0;
-		putuint(numero++);
-		while (contador < limite) {
-			contador++;
+		counter = 0;
+		putuint(number_to_display++);
+		while (counter < limit) {
+			counter++;
 		}
 	}
 }
