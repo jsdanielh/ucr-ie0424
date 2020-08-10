c6b23315e53933ffd47334bc54fc366da874da63
Jose Daniel Hernandez <jsdaniel.h@gmail.com>
2020-08-10 00:48:53 -0600

    Make firmware.c a symbolic link
    Make firmware.c a symbolic link so students can change it to
    compile different versions according to a specific laboratory
    section.
    
    Signed-off-by: Jose Daniel Hernandez <jsdaniel.h@gmail.com>

---
+++ src/firmware/firmware_lab1_part1.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+
+static void putuint(uint32_t i) {
+	*((volatile uint32_t *)0x10000000) = i;
+}
+
+void main() {
+	uint32_t number_to_display = 0;
+	uint32_t counter = 0;
+	const uint32_t limit = 1000000;
+
+	while (1) {
+		counter = 0;
+		putuint(number_to_display);
+		number_to_display++;
+		while (counter < limit) {
+			counter++;
+		}
+	}
+}
