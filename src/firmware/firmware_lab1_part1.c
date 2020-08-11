4f23ac89771e4db9deadbab8838c17ab02b24e0e
Jose Daniel Hernandez <jsdaniel.h@gmail.com>
2020-08-11 03:24:51 +0000

    Use defines for constants
    Use defines for constants in the firmware code.
    
    Signed-off-by: Jose Daniel Hernandez <jsdaniel.h@gmail.com>

--- src/firmware/firmware_lab1_part1.c
+++ src/firmware/firmware_lab1_part1.c
@@ -1,19 +1,21 @@
 #include <stdint.h>
 
+#define LED_REGISTERS_MEMORY_ADD 0x10000000
+#define LOOP_WAIT_LIMIT 100
+
 static void putuint(uint32_t i) {
-	*((volatile uint32_t *)0x10000000) = i;
+	*((volatile uint32_t *)LED_REGISTERS_MEMORY_ADD) = i;
 }
 
 void main() {
 	uint32_t number_to_display = 0;
 	uint32_t counter = 0;
-	const uint32_t limit = 1000000;
 
 	while (1) {
 		counter = 0;
 		putuint(number_to_display);
 		number_to_display++;
-		while (counter < limit) {
+		while (counter < LOOP_WAIT_LIMIT) {
 			counter++;
 		}
 	}
